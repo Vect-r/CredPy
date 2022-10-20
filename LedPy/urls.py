6cf78c1b287d69a80b719600fcadc1116f48a7df
Mitesh <47442331+Vect-r@users.noreply.github.com>
2022-10-20 21:19:20 +0530

    Update urls.py

--- LedPy/urls.py
+++ LedPy/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+from django.views.static import static
+from django.conf.urls import urls
+
 admin.site.site_header= "Ledpy Admin"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",include('home.urls'))
+    path("",include('home.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
