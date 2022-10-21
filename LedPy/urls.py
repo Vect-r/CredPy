9475d90c119c80b3c577c5ac034bb7f90a140f9e
Mitesh <47442331+Vect-r@users.noreply.github.com>
2022-10-22 01:05:27 +0530

    Add files via upload

--- LedPy/urls.py
+++ LedPy/urls.py
@@ -15,15 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
 
-from django.views.static import static
-from django.conf.urls import urls
+from django.views.static import serve
+from django.urls import re_path as url
 
 admin.site.site_header= "Ledpy Admin"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include('home.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root':settings.STATIC_ROOT}), 
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
+
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
